8b01fd235232779e22d6509550567e52643e75c2
Sam Grimsley <99296361+SGrimsley02@users.noreply.github.com>
2024-04-13 16:22:26 -0500

    Create inputParser.cpp
    Added brief outline

---
+++ BoolLogic_Codebase/inputParser.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/* 
+This file should define an input parser. It should be able to scan for inputs, then
+take it in as a string. After that, it should transform it into a usable form for the expression parser.
+  This may or may not be a string, it will depend on implementation of expression parser.
+  If there are characters not necessary for the expression parser, then these characters should be removed
+  by the input parser.
+*/
