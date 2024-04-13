b850e315b99bff4ab27a15df1a748712fb20b222
Sam Grimsley <99296361+SGrimsley02@users.noreply.github.com>
2024-04-13 16:34:30 -0500

    Create evaluator.cpp
    Added brief description

---
+++ BoolLogic_Codebase/evaluator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*
+This is where the actual evaluation occurs. It should be able to take an input from exp parser,
+then give a boolean output corresponding to the original equation.
+    Note: While variable inputs can and should be in the final product, they will take a somewhat
+    different approach to implement since the final output will likely not be just True or False.
+    As such, start with the simpler case, figure out what's needed for that, then build from there.
+The output of this should be able to be passed back to the interface to be shown to the user.
+*/
