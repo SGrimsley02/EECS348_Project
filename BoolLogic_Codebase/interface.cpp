84a99bbf3ab4361e952f683067678ac290460a99
Sam Grimsley <99296361+SGrimsley02@users.noreply.github.com>
2024-04-13 16:39:17 -0500

    Create interface.cpp
    Added brief description

---
+++ BoolLogic_Codebase/interface.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+This program will handle any and all interaction with the user. It should be able to ask the user for input
+before passing off to the input parser, then it should send the output of the evaluator back to the user
+in a readable format.
+This should also have a way of handling any errors that may arise, giving the user a message saying what went
+wrong without breaking the program.
+Lastly, this should have a way to end the program.
+All of this should be done with a readable interface. While it does not need to be overly complicated,
+any method that will allow the user to easily understand what is going on and what they can do is acceptable.
+This may mean a commandline interface, or it could also be a graphical interface of some kind. The primary concern
+is readability and functionality.
+*/
